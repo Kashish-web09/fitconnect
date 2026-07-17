cf9e8b9d237d054a19f3e3420a3d3326bae36521
Kashish Narang <knarang0909@gmail.com>
2026-07-17 15:10:36 +0530

    Switch email service from Brevo to Gmail SMTP

--- config/mailer.js
+++ config/mailer.js
@@ -1,16 +1,22 @@
-import dotenv from "dotenv";
-dotenv.config();
+import nodemailer from "nodemailer";
 
-import nodemailer from 'nodemailer';
+const transport = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.APP_PASS,
+  },
+});
 
-const transport=nodemailer.createTransport({
-    host:'smtp.gmail.com',
-    port:587,
-    secure:false,
-    auth:{
-        user:process.env.SENDER_EMAIL,
-        pass:process.env.APP_PASS
-    }
-    
+transport.verify((err, success) => {
+  if (err) {
+    console.error("VERIFY ERROR:", err);
+  } else {
+    console.log("SMTP Ready");
+  }
 });
 export default transport;
+
+
